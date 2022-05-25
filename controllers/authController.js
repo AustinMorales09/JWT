40720c5a678594150330b6be3d9b7d1d9d001a70
Austin <Harrenaustint@gmail.com>
2022-05-25 13:29:38 -0400

    push

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,10 @@
 const User = require('../models/User');
 
+const handleErrors = (err) => {
+    console.log(err.message, err.code);
+}
+
+
 module.exports.signup_get = (req,res) =>{
     res.render('signup');
 }
@@ -13,7 +18,7 @@ module.exports.signup_post = async (req,res) =>{
      res.status(201).json(user);
     }
     catch(err){
-        console.log(err);
+        handleErrors(err);
         res.status(400).send('error, user wasnt created');
     }
 
