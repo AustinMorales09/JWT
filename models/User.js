dae1ce9a14eccb31ecc2b43af3f3dc2f8e12c03e
Austin <Harrenaustint@gmail.com>
2022-05-27 11:00:56 -0400

    update

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const {isEmail} = require('validator');
-
+const bcrypt = require('bcrypt');
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -14,6 +14,17 @@ const userSchema = new mongoose.Schema({
         required: [true,'Please enter a password'],
         minLength: [6, 'Minimum length is 6 characters']
     }
+})
+
+// userSchema.post('save', function(doc,next){
+//     console.log('new user was created and saved', doc);
+//     next();
+// })
+userSchema.pre('save', async function(next){
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    // console.log('user about to be created and saved', this);
+    next();
 })
 
 const User = mongoose.model('user', userSchema);
